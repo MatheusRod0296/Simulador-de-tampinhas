80696ed354a5d7b97c7586b480898e2c38215105
matheus <matheusrodrigues0296@gmail.com>
2023-06-18 20:16:49 -0300

    refactor

--- script.js
+++ script.js
@@ -1,149 +1,175 @@
 $(document).ready(function(){
-    var QtdTimes = 0;
-    var QtdJogadoresPorTime = 0;
-    var TotalJogadores = 0;
-    var JogadoresQueSobraram = 0;
+    var _totalTeams = 0;
+    var _totalPlayersPerTeam = 0;
+    var _totalPlayers = 0;
+    var _restOfPlayers = 0;
 
-    var CoresEscolhidas = [];
-    var VagasPorCores = [];
-    var soretioCount = 0;
+    var _colorsChosen = [];
+    var _spotsPerColor = [];
+    var _sortCount = 0;
+    var _colorsCount = 0;
  
-    $("#calcular").click(function(){
+    $("#calculate").click(function(){
         reset();
-        TotalJogadores =$("#totalJogadores").val(); 
-
-        calcularQuantidadeTimes();
-        showQuantidade();
-        showQuantidade();
-        
-      });    
-        
-    $("input[name='chk']").on('change', function(evt) { 
-        if ($('input[type=checkbox]:checked').length > QtdTimes) {
-            $(this).prop('checked', false);
-
-            alert("apenas:  "+ QtdTimes);
-        }else {
-           if($(this).prop('checked')){              
-                CoresEscolhidas.push(this.attributes["data"].value);
-           }else{
-               reset();               
-           }           
-        }
-
-        if ($('input[type=checkbox]:checked').length == QtdTimes && QtdTimes > 0) {            
-            for (let index = 0; index < CoresEscolhidas.length; index++) {
-                if(JogadoresQueSobraram > 0 && index == CoresEscolhidas.length -1){
-                    VagasPorCores.push({ cor : CoresEscolhidas[index], vagas : JogadoresQueSobraram, vagasOcupadas: 0})  
-                }else{
-                    VagasPorCores.push({ cor : CoresEscolhidas[index], vagas : QtdJogadoresPorTime, vagasOcupadas: 0})  
-                }                            
-            }
-
-            mostrarOrdemDosTimes();
-        }
-    });
-
-    $("#btn_sortear").on('click', function() {   
-        if(soretioCount >= TotalJogadores) {
-            alert("Times preenchidos!");
+        _totalPlayers = $("#players").val(); 
+        _totalPlayersPerTeam = $("#playersPerTeam").val();
+        if(_totalPlayers <= 0 || _totalPlayersPerTeam <= 0){
+            alert("Preencha os campos a cima!");
             return;
         }
 
-        if(CoresEscolhidas.length < QtdTimes){
+        _restOfPlayers = calculateRestOfPlayers(_totalPlayers, _totalPlayersPerTeam);
+
+        _totalTeams = calculateAmountOfTeams(_totalPlayers, _totalPlayersPerTeam, _restOfPlayers);
+        
+        showNumberOfTeams(_totalTeams); 
+      })   
+        
+    $("input[name='chk']").on('change', function() { 
+        if(CanCheck(this, _totalTeams, _colorsChosen)){
+            if ($('input[type=checkbox]:checked').length == _totalTeams && _totalTeams > 0){
+                buildSpotsPerColor(_colorsChosen, _restOfPlayers, _spotsPerColor, _totalPlayersPerTeam);
+                showTeamsOrder(_spotsPerColor);
+            }
+    
+            _colorsCount = _colorsChosen.length;
+        }
+    })   
+
+    $("#btn_sort").on('click', function() {   
+        if(_colorsCount == 0 ||_colorsCount < _totalTeams){
             alert("Selecione os times!");
             return;
         }
-            
-        if(CoresEscolhidas.length > 0){
-            var max = CoresEscolhidas.length; 
-            var sort = Math.floor(Math.random() * max);
-            
-            var index = verificarTimeDisponivel(sort);
-           
-            VagasPorCores[index].vagasOcupadas++;
-            soretioCount++;            
 
-            mostrarCoreSorteada(VagasPorCores[index].cor);
-            mostrarStatus(VagasPorCores[index].cor); 
-            atualizaVagasOcupadas(VagasPorCores[index]);           
-        }                   
+        if(_sortCount >= _totalPlayers) {
+            alert("Times preenchidos!");
+            return;
+        }        
             
-    }); 
+        if(_colorsChosen.length > 0){            
+            let indexOfSpot = sort(_colorsChosen, _spotsPerColor);
+
+            showSortedColor(_spotsPerColor[indexOfSpot].color);
+            showStatus(_spotsPerColor[indexOfSpot].color); 
+            updateSpotsAvailables(_spotsPerColor[indexOfSpot]);  
+            _sortCount++;         
+        } 
+    })
     
-    function calcularQuantidadeTimes(){
-        QtdJogadoresPorTime = $("#qtdJogadoresPorTime").val();           
-
-        if(QtdJogadoresPorTime > 0 && TotalJogadores > 0){
-            JogadoresQueSobraram = TotalJogadores % QtdJogadoresPorTime;
-
-            var resultadoDivisao = TotalJogadores / QtdJogadoresPorTime;
-    
-            var resultado = Math.trunc(resultadoDivisao);
+    function calculateAmountOfTeams(totalPlayers, totalPlayersPerTeam, restOfPlayers){
+        if(totalPlayersPerTeam > 0 && totalPlayers > 0){  
+            var totalTeams = Math.trunc(totalPlayers / totalPlayersPerTeam);
             
-            if(JogadoresQueSobraram > 0 ){
-                resultado++;
+            if(restOfPlayers > 0 ){
+                totalTeams++;
             }        
             
-            if(resultado > 9){
+            if(totalTeams > 9){
                 alert("Quantidade de times nao pode ultapassar 9!");
                 return;
             }
             
-            QtdTimes = resultado;           
+           return totalTeams;          
         } 
     }
 
-    function showQuantidade(){
-        $("#lbl_selecione").empty();
-        $("#lbl_selecione").prepend("Selecione "+ QtdTimes + " times:");
+    function calculateRestOfPlayers(totalPlayers, totalPlayersPerTeam){
+        if(totalPlayersPerTeam > 0 && totalPlayers > 0){
+            return totalPlayers % totalPlayersPerTeam;
+        }              
     }
 
-    function reset(){
-        $('input[type=checkbox]').prop('checked',false);
-        $("#list-vagas").empty();
-        $("#div_status").empty();
-        $("#div_cor_sorteada").css("background-color", "white");
-        VagasPorCores = [];
-        CoresEscolhidas = [];
-        soretioCount = 0;
+    function showNumberOfTeams(totalTeams){
+        $("#lbl_select").empty();
+        $("#lbl_select").prepend("Selecione "+ totalTeams + " times:");
     }
 
-    function verificarTimeDisponivel(sort){
-        if(VagasPorCores[sort].vagasOcupadas < VagasPorCores[sort].vagas){
-            return sort;
+    function sort(colorsChosen, spotsPerColor ){
+        let sort = Math.floor(Math.random() * colorsChosen.length);
+        var sortedColor = colorsChosen[sort];
+         
+        var indexOfSpot =  spotsPerColor.map( e => e.color).indexOf(sortedColor);
+        
+        let available = verifyAvailableTeam(indexOfSpot, spotsPerColor);
+        if(available){
+            spotsPerColor[indexOfSpot].spotsFilled++;
+            return indexOfSpot;
         }else{
-            for (let index = 0; index < VagasPorCores.length; index++) {
-                if(VagasPorCores[index].vagasOcupadas < VagasPorCores[index].vagas){
-                    return index;
-                }                
-            }
+            colorsChosen.splice(sort, 1);         
+            return recursiveSort(colorsChosen, spotsPerColor);
+        } 
+    }
+
+    function recursiveSort(colorsChosen, spotsPerColor){
+        return sort(colorsChosen, spotsPerColor);
+    }
+
+    function verifyAvailableTeam(indexOfSpot, spotsPerColor, sortedColor){       
+        
+        if(indexOfSpot > -1 && spotsPerColor[indexOfSpot].spotsFilled < spotsPerColor[indexOfSpot].spotsAvailable){
+            return true;
         }
+        
+        return false;       
     }
     
-    function mostrarCoreSorteada(cor){  
-        $("#div_cor_sorteada").css("display", "none");
-        $("#div_cor_sorteada").css("background-color", cor);
-        $("#div_cor_sorteada").delay(500).fadeIn();       
+    function showSortedColor(cor){  
+        $("#sorted_color").css("display", "none");
+        $("#sorted_color").css("background-color", cor);
+        $("#sorted_color").delay(50).fadeIn();       
     }   
 
-    function mostrarStatus(cor){  
-        $("#div_status").prepend("<div class='square-status' style='background-color: "+cor+"'></div>");       
+    function showStatus(cor){  
+        $("#status").prepend("<div class='cap-status' style='background-color: "+ cor +"'></div>");       
     } 
 
-    function mostrarOrdemDosTimes(){
-        VagasPorCores.forEach(element => {
-            if(element.cor == "yellow" || element.cor == "pink" || element.cor == "orange" || element.cor == "greenyellow"){
-                $("#list-vagas").append('<li id="li_'+ element.cor +'" class="list-group-item row_status color-black" style="background-color: '+element.cor+'">Vagas:  0/'+element.vagas+'</li>');
+    function CanCheck(input, totalTeams, colorsChosen){
+        if ($('input[type=checkbox]:checked').length > totalTeams) {
+            $(input).prop('checked', false);
+            alert("apenas:  "+ totalTeams); 
+            return false;           
+        }else if($(input).prop('checked')) {  
+            colorsChosen.push(input.attributes["data"].value);
+            return true;             
+        }else {
+            reset();
+            return false;
+        }        
+    }
+
+    function buildSpotsPerColor(colorsChosen, restOfPlayers, spotsPerColor, totalPlayersPerTeam){               
+        for (let index = 0; index < colorsChosen.length; index++) {
+            if(restOfPlayers > 0 && index == colorsChosen.length -1){
+                spotsPerColor.push({ color : colorsChosen[index], spotsAvailable : restOfPlayers, spotsFilled: 0})  
             }else{
-                $("#list-vagas").append('<li id="li_'+ element.cor +'" class="list-group-item row_status color-white" style="background-color: '+element.cor+'">Vagas:  0/'+element.vagas+'</li>');
+                spotsPerColor.push({ color : colorsChosen[index], spotsAvailable : totalPlayersPerTeam, spotsFilled: 0})  
+            }                            
+        }
+    }
+
+    function showTeamsOrder(spotsPerColor){
+        spotsPerColor.forEach(spot => {
+            if(spot.color == "yellow" || spot.color == "pink" || spot.color == "orange" || spot.color == "greenyellow"){
+                $("#list-spots").append('<li id="li_'+ spot.color +'" class="list-group-item row_status text-black" style="background-color: '+ spot.color +'">Vagas:  0/'+ spot.spotsAvailable +'</li>');
+            }else{
+                $("#list-spots").append('<li id="li_'+ spot.color +'" class="list-group-item row_status text-white" style="background-color: '+ spot.color +'">Vagas:  0/'+ spot.spotsAvailable +'</li>');
             }
         });  
     }
 
-    function atualizaVagasOcupadas(vagas){
-        $("#li_"+ vagas.cor).empty();
-        $("#li_"+ vagas.cor).append("Vagas:  " +vagas.vagasOcupadas+"/" +vagas.vagas);
-    }    
+    function updateSpotsAvailables(spotsPerColor){
+        $("#li_"+ spotsPerColor.color).empty();
+        $("#li_"+ spotsPerColor.color).append("Vagas:  " + spotsPerColor.spotsFilled +"/" + spotsPerColor.spotsAvailable);
+    } 
+    
+    function reset(){
+        $('input[type=checkbox]').prop('checked',false);
+        $("#list-spots").empty();
+        $("#status").empty();
+        $("#sorted_color").css("background-color", "white");
+        _spotsPerColor = [];
+        _colorsChosen = [];
+        _sortCount = 0;
+    }
 });
-
